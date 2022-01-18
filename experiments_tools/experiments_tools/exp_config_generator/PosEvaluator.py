0916650009a3b6b64aefc950641aa0978156e1fc
Vitor Hugo <vitormarzarotto@hotmail.com>
2022-01-18 15:01:52 -0300

    experimentos do humanoid

--- experiments_tools/experiments_tools/exp_config_generator/PosEvaluator.py
+++ experiments_tools/experiments_tools/exp_config_generator/PosEvaluator.py
@@ -1,4 +1,4 @@
 import os
 
 for x in range(1,11):
-    os.system(f"python3.7 ../../../../../bin/es.py -f Walker2DBulletEnv-v0-3.ini -g bestgS{x}.npy -p ")
+    os.system(f"python3.7 ../../../../../bin/es.py -f humanoid.ini -g bestgS{x}.npy -p ")
